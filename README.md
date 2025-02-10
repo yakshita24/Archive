bd8335d69d850d4239c1b690c10fe60e063abb1e
yakshita24 <yakshitaacharya2412@gmail.com>
2025-02-11 01:00:44 +0530

    Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Welcome to the AI Course Master project!
+
+To Run:
+
+Step1: npm install
+Step2: npm run dev
+# ai-master
+# AI-Course-Project---YASH
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <Button>I am AI powered your teacher. Let me how can i help you?</Button>;
+}
